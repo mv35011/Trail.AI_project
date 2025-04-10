3a79478673eef3f73096e090c4a96d6041882f5c
mv35011 <manmohanv.ug23.ec@nitp.ac.in>
2025-04-10 14:45:18 +0530

    more initial changes

--- app/auth.py
+++ app/auth.py
@@ -1,21 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Flask
 
-auth = Blueprint('auth', __name__)
+auth = Flask(__name__)
+
+auth.route('/')
 
 
-@auth.route('/login')
 def login():
-    pass
-
-
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
-
-@auth.route('/sign-up')
-def sign_up():
-    return render_template("sign_up.html")
-
-@auth.route('/sign-in')
-def sign_in():
     pass
